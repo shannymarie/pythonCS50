8460c0fcfefa68deb1b82c7962e6dc5b4e98aea3
unknown <fenico10@hotmail.nl>
2023-05-24 11:51:12 -0300

    placed comments

--- sequences.py
+++ sequences.py
@@ -1,3 +1,15 @@
 name = "Harry"
 
 print(name[0])
+
+#this is al list 
+names = ["Harry", "Ron","Her","Ginny"]
+
+print(names[0])
+# end of the list code 
+
+#list - seequence of mutable values
+#tuple - sequence of immutable values
+#set - collection of unique values
+#dict - collection of key value pairs
+
