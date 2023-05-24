51c82ce924f8e54cee84154fb821f9459d7f347f
unknown <fenico10@hotmail.nl>
2023-05-24 11:56:35 -0300

    adding list functions

--- sequences.py
+++ sequences.py
@@ -5,7 +5,17 @@ print(name[0])
 #this is al list 
 names = ["Harry", "Ron","Her","Ginny"]
 
+print(names)
 print(names[0])
+
+#adding a value in a list using append
+names.append("Draco")
+
+# "sort" sorts the list
+names.sort()
+
+print(names)
+
 # end of the list code 
 
 #list - seequence of mutable values
