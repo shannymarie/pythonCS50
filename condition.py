5abe564a46992755b3281402c2785f499b8925a4
unknown <fenico10@hotmail.nl>
2023-05-24 11:40:24 -0300

    upload python files

---
+++ condition.py
@@ -0,0 +1,9 @@
+number = int(input("type a number:"))
+
+if number > 0:
+    print(f"{number} is positive")
+elif number < 0:
+    print(f"{number} is negative")
+else: 
+    print("your number is 0")
+
