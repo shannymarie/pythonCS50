838a24b352d5ef46f4d6ba0d1ee5d9bf72e594d2
unknown <fenico10@hotmail.nl>
2023-05-24 19:29:19 -0300

    adding the loop file

---
+++ loops.py
@@ -0,0 +1,20 @@
+# this is a for loop
+for i in [1,2,3,4,5]:
+    print(i)
+
+# built in range function
+for i in range(6):
+    print(i+1)
+
+# loop for strings
+names = ["Harry","Ron","Hermaini"]
+
+for name in names:
+    print(name)
+
+#loop in characters
+
+name = "Harry"
+
+for character in name:
+    print(character)
