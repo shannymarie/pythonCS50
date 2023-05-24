863292808f3ecec029a97a9034bc24f12815ed6d
unknown <fenico10@hotmail.nl>
2023-05-24 20:16:25 -0300

    adding decorators file

---
+++ decorators.py
@@ -0,0 +1,13 @@
+# decorator functions takes a function as 
+def announce(f):
+    def wrapper():
+        print("About to run the function...")
+        f()
+        print("Done with the function")
+    return wrapper
+    
+@announce
+def hello():
+    print("Hello, world")
+
+hello()
